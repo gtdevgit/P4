39f6fd3d4a54f0bd3537ddb2c4f1bebf0c9bab39
Guillaume Toussaint <gtdev.git@gmail.com>
2021-01-08 09:47:43 +0100

    ajout tests unitaires

--- Mareu/app/src/test/java/com/lamzone/mareu/MareuUnitTest.java
+++ Mareu/app/src/test/java/com/lamzone/mareu/MareuUnitTest.java
@@ -18,8 +18,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
@@ -27,6 +29,7 @@ import java.util.ListIterator;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -95,20 +98,54 @@ public class MareuUnitTest {
     @Test
     public void nbReunionParSalle() {
         ReunionApiService service = DI.getNewInstanceApiService();
-        int nb = service.nbReunionParSalle(1L);
-        assertEquals(nb, 3);
+        int count = service.nbReunionParSalle(1L);
+        assertEquals(3, count);
     }
 
     @Test
-    public void findSalleById()
-    {
+    public void findSalleById() {
         ReunionApiService service = DI.getNewInstanceApiService();
         Salle firstSalle = service.getSalles().get(0);
         Salle findSalle = service.findSalleById(1L);
         assertEquals(firstSalle, findSalle);
     }
 
-    // void filtrerReunionsParSalle(long idSalle);
-    // void filtrerReunionsParDate(Date date);
-    // void supprimerFiltreReunion();
+    @Test
+    public void filtrerReunionsParSalle() {
+        ReunionApiService service = DI.getNewInstanceApiService();
+        service.filtrerReunionsParSalle(1L);
+        // doit trouver 3 réunions, toutes les 3 sur la salle 1
+        assertEquals(3, service.getReunions().size());
+        long idSalle = service.getReunions().get(0).getIdSalle();
+        assertEquals(1l, idSalle);
+        idSalle = service.getReunions().get(1).getIdSalle();
+        assertEquals(1l, idSalle);
+        idSalle = service.getReunions().get(2).getIdSalle();
+        assertEquals(1l, idSalle);
+    }
+
+    @Test
+    public void filtrerReunionsParDate(){
+        ReunionApiService service = DI.getNewInstanceApiService();
+        Date date = new GregorianCalendar(2020, Calendar.DECEMBER, 10, 10, 30).getTime();
+        service.filtrerReunionsParDate(date);
+        List<Reunion> reunion = service.getReunions();
+        // doit trouver 1 réunions,
+        assertEquals(1, service.getReunions().size());
+        // la date doit correspondre
+        Date dateDebut = service.getReunions().get(0).getDateDebut();
+        assertEquals(date, dateDebut);
+    }
+
+    @Test
+    public void supprimerFiltreReunion(){
+        ReunionApiService service = DI.getNewInstanceApiService();
+        int countTotal = service.getReunions().size();
+        service.filtrerReunionsParSalle(1L);
+        int countSalle = service.getReunions().size();
+        assertNotEquals(countSalle, countTotal);
+        service.supprimerFiltreReunion();
+        int countNew = service.getReunions().size();
+        assertEquals(countNew, countTotal);
+    };
 }
